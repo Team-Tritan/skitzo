429c9d45ea65d5353b71d7bcafb870fedce00498
xfy777 <sysop777@proton.me>
2023-06-22 04:47:55 -0400

    ?

--- static/scripts/patience.js
+++ static/scripts/patience.js
@@ -1,11 +1,11 @@
 function showElapsedTime() {
   var start = new Date();
-  var loader = document.getElementById("loader");
+  var loader = document.getElementById("secs");
 
   setInterval(function () {
     var now = new Date();
     var elapsedSeconds = Math.floor((now - start) / 1000);
-    loader.textContent = "Loading... " + elapsedSeconds + "s";
+    loader.textContent = "Waited " + elapsedSeconds + " seconds.";
   }, 1000);
 }
 
